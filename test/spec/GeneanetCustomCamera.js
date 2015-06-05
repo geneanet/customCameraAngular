f8ecdcdee9fe2d873bd2520293ac4a10ec64d5d7
Christophe BOUCAUT <christophe.boucaut@geneanet.org>
2015-06-05 17:35:14 +0200

    Complete the test to configure the service.

--- test/spec/GeneanetCustomCamera.js
+++ test/spec/GeneanetCustomCamera.js
@@ -3,15 +3,41 @@
 describe('geneanetCustomCamera', function() {
     beforeEach(module('geneanetCustomCamera'));
 
-    beforeEach(module(function(geneanetCustomCameraProvider) {
-        return function() {
-            geneanetCustomCameraProvider.setOptionMiniature(false);
-        };
-    }));
+    it('should have options values as values set by the provider', function() {
+        var optionMiniature = false;
+        var optionSaveInGallery = true;
+        var optionCameraBackgroundColor = '#012345';
+        var optionCameraBackgroundColorPressed = '#678901';
+        var optionQuality = '12';
+        var optionOpacity = false;
+        var optionSwitchFlash = true;
+        var optionSwitchCamera = false;
 
-    it('should have the miniature option disabled.', function() {
-        inject(function(geneanetCustomCamera) {
-            expect(geneanetCustomCamera.getOptionMiniature()).not.toBeTruthy();
+        // init service
+        module(function(geneanetCustomCameraProvider) {
+            return function() {
+                geneanetCustomCameraProvider.setOptionMiniature(optionMiniature);
+                geneanetCustomCameraProvider.setOptionSaveInGallery(optionSaveInGallery);
+                geneanetCustomCameraProvider.setOptionCameraBackgroundColor(optionCameraBackgroundColor);
+                geneanetCustomCameraProvider.setOptionCameraBackgroundColorPressed(optionCameraBackgroundColorPressed);
+                geneanetCustomCameraProvider.setOptionQuality(optionQuality);
+                geneanetCustomCameraProvider.setOptionOpacity(optionOpacity);
+                geneanetCustomCameraProvider.setOptionSwitchFlash(optionSwitchFlash);
+                geneanetCustomCameraProvider.setOptionSwitchCamera(optionSwitchCamera);
+            };
         });
+
+        inject(function(geneanetCustomCamera) {
+            expect(geneanetCustomCamera.getOptionMiniature()).toEqual(optionMiniature);
+            expect(geneanetCustomCamera.getOptionSaveInGallery()).toEqual(optionSaveInGallery);
+            expect(geneanetCustomCamera.getOptionCameraBackgroundColor()).toEqual(optionCameraBackgroundColor);
+            expect(geneanetCustomCamera.getOptionCameraBackgroundColorPressed()).toEqual(optionCameraBackgroundColorPressed);
+            expect(geneanetCustomCamera.getOptionQuality()).toEqual(optionQuality);
+            expect(geneanetCustomCamera.getOptionOpacity()).toEqual(optionOpacity);
+            expect(geneanetCustomCamera.getOptionSwitchFlash()).toEqual(optionSwitchFlash);
+            expect(geneanetCustomCamera.getOptionSwitchCamera()).toEqual(optionSwitchCamera);
+        });
+
+        // @TODO: test set default flash
     });
 });
