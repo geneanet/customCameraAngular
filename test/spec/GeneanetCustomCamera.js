035978a46d0444e05a6e7585100f7561df5d4055
Christophe BOUCAUT <christophe.boucaut@geneanet.org>
2015-06-08 09:38:31 +0200

    Add tests to check the service configuration

--- test/spec/GeneanetCustomCamera.js
+++ test/spec/GeneanetCustomCamera.js
@@ -1,14 +1,28 @@
 'use strict';
 
-describe('geneanetCustomCamera', function() {
+describe('Configuration geneanetCustomCamera', function() {
     beforeEach(module('geneanetCustomCamera'));
+
+    beforeEach(function() {
+        navigator.GeneanetCustomCamera = {
+            FlashModes: {
+                ACTIVE: 0,
+                DISABLE: 1,
+                AUTO: 2
+            },
+            CameraFacings: {
+                BACK: 0,
+                FRONT: 1
+            }
+        };
+    });
 
     it('should have options values as values set by the provider', function() {
         var optionMiniature = false;
         var optionSaveInGallery = true;
         var optionCameraBackgroundColor = '#012345';
         var optionCameraBackgroundColorPressed = '#678901';
-        var optionQuality = '12';
+        var optionQuality = 12;
         var optionOpacity = false;
         var optionSwitchFlash = true;
         var optionSwitchCamera = false;
@@ -37,7 +51,65 @@ describe('geneanetCustomCamera', function() {
             expect(geneanetCustomCamera.getOptionSwitchFlash()).toEqual(optionSwitchFlash);
             expect(geneanetCustomCamera.getOptionSwitchCamera()).toEqual(optionSwitchCamera);
         });
+    });
 
-        // @TODO: test set default flash
+    it('should have the flash disabled by default', function() {
+        module(function(geneanetCustomCameraProvider) {
+            return function() {
+                geneanetCustomCameraProvider.optionDefaultFlashIsDisabled();
+            };
+        });
+
+        inject(function(geneanetCustomCamera) {
+            expect(geneanetCustomCamera.getOptionDefaultFlash()).toEqual(navigator.GeneanetCustomCamera.FlashModes.DISABLE);
+        });
+    });
+
+    it('should have the flash actived by default', function() {
+        module(function(geneanetCustomCameraProvider) {
+            return function() {
+                geneanetCustomCameraProvider.optionDefaultFlashIsActived();
+            };
+        });
+
+        inject(function(geneanetCustomCamera) {
+            expect(geneanetCustomCamera.getOptionDefaultFlash()).toEqual(navigator.GeneanetCustomCamera.FlashModes.ACTIVE);
+        });
+    });
+
+    it('should have the flash in the auto mode by default', function() {
+        module(function(geneanetCustomCameraProvider) {
+            return function() {
+                geneanetCustomCameraProvider.optionDefaultFlashIsAuto();
+            };
+        });
+
+        inject(function(geneanetCustomCamera) {
+            expect(geneanetCustomCamera.getOptionDefaultFlash()).toEqual(navigator.GeneanetCustomCamera.FlashModes.AUTO);
+        });
+    });
+
+    it('should use the back camera by default', function() {
+        module(function(geneanetCustomCameraProvider) {
+            return function() {
+                geneanetCustomCameraProvider.optionDefaultCameraIsBack();
+            };
+        });
+
+        inject(function(geneanetCustomCamera) {
+            expect(geneanetCustomCamera.getOptionDefaultCamera()).toEqual(navigator.GeneanetCustomCamera.CameraFacings.BACK);
+        });
+    });
+
+    it('should use the front camera by default', function() {
+        module(function(geneanetCustomCameraProvider) {
+            return function() {
+                geneanetCustomCameraProvider.optionDefaultCameraIsFront();
+            };
+        });
+
+        inject(function(geneanetCustomCamera) {
+            expect(geneanetCustomCamera.getOptionDefaultCamera()).toEqual(navigator.GeneanetCustomCamera.CameraFacings.FRONT);
+        });
     });
 });
