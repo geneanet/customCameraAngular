b92eaf39dc72926482523676b9624c6d317a9bd8
Christophe BOUCAUT <christophe.boucaut@geneanet.org>
2015-06-08 10:12:10 +0200

    Add tests on geneanetCustomCamera.encodeBase64FromImg

--- test/spec/GeneanetCustomCamera.js
+++ test/spec/GeneanetCustomCamera.js
@@ -113,3 +113,38 @@ describe('Configuration geneanetCustomCamera', function() {
         });
     });
 });
+
+describe('geneanetCustomCamera.encodeBase64FromImg', function() {
+    var geneanetCustomCamera;
+
+    beforeEach(function() {   
+        module('geneanetCustomCamera');
+
+        inject(function(_geneanetCustomCamera_) {
+            geneanetCustomCamera = _geneanetCustomCamera_;
+        });
+    });
+
+    it('should exist and it\'s a function', function() {
+        var encodeBase64FromImg = geneanetCustomCamera.encodeBase64FromImg;
+        
+        expect(encodeBase64FromImg).toBeDefined();
+        expect(encodeBase64FromImg).toEqual(jasmine.any(Function));
+    });
+
+    it('should return the base64 of image', function() {
+        var img = document.createElement("img");
+        var base64 = geneanetCustomCamera.encodeBase64FromImg(img);
+
+        expect(base64).toEqual(jasmine.any(String));
+        expect(base64.length).not.toEqual(0);
+    });
+
+    it('should return the base64 of image without the starting: data:.*,', function() {
+        var img = document.createElement("img");
+        var base64 = geneanetCustomCamera.encodeBase64FromImg(img, null, true);
+
+        expect(base64).toEqual(jasmine.any(String));
+        expect(base64.length).toEqual(0);
+    });
+});
